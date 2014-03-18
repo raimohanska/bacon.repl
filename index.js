a75e833a1d7835394ddefa345a9467cfbea983a9
Juha Paananen <juha.paananen@gmail.com>
2014-03-18 23:23:42 +0200

    move web stuff under public/

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
+    response.sendfile(__dirname + '/public/index.html');
 }).configure(function() {
-    app.use('/', express.static(__dirname + '/'));
+    app.use('/', express.static(__dirname + '/public'));
+    app.use('/jsrepl', express.static(__dirname + '/jsrepl'));
+    app.use('/node_modules', express.static(__dirname + '/node_modules'));
+    app.use('/extern', express.static(__dirname + '/extern'));
 }).listen(port);
